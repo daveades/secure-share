ce2cb3783e72496100c5b7e664ee6eaca8cab8f8
DAVE <jesugbohunmi23@gmail.com>
2025-07-07 21:53:43 +0100

    Refactor MongoDB setup: replace Dockerfile and initialization scripts with init-app-user.js for user creation; update docker-compose.yaml to streamline service configuration.

---
+++ mongodb/init-app-user.js
@@ -0,0 +1,6 @@
+db = db.getSiblingDB(process.env.DB_NAME);
+db.createUser({
+  user: process.env.MONGO_APP_USERNAME,
+  pwd:  process.env.MONGO_APP_PASSWORD,
+  roles:[{role:"readWrite",db:process.env.DB_NAME}]
+});
