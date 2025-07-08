4c220c19ac0dc9afd8eb69fb1bcab5467faa5342
DAVE <jesugbohunmi23@gmail.com>
2025-07-08 02:55:26 +0100

    Add file API methods for upload/download

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -39,3 +39,75 @@ api.interceptors.response.use(
 );
 
 export default api;
+
+// File API methods
+export const fileAPI = {
+    // Upload a file
+    uploadFile: async (file, options = {}) => {
+        const formData = new FormData();
+        formData.append('file', file);
+        
+        if (options.expirationHours) {
+            formData.append('expiration_hours', options.expirationHours);
+        }
+        if (options.password) {
+            formData.append('password', options.password);
+        }
+        if (options.downloadLimit) {
+            formData.append('download_limit', options.downloadLimit);
+        }
+        
+        return api.post('/files/upload', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+            onUploadProgress: options.onProgress,
+        });
+    },
+
+    // Get user's files
+    getMyFiles: async (includeExpired = false) => {
+        return api.get(`/files/my-files?include_expired=${includeExpired}`);
+    },
+
+    // Get file info
+    getFileInfo: async (fileId) => {
+        return api.get(`/files/info/${fileId}`);
+    },
+
+    // Download file
+    downloadFile: async (fileId, password = null) => {
+        const config = {
+            responseType: 'blob',
+        };
+        
+        if (password) {
+            return api.post(`/files/download/${fileId}`, { password }, config);
+        } else {
+            return api.get(`/files/download/${fileId}`, config);
+        }
+    },
+
+    // Delete file
+    deleteFile: async (fileId) => {
+        return api.delete(`/files/delete/${fileId}`);
+    },
+
+    // Get shared file info
+    getSharedFileInfo: async (accessToken) => {
+        return api.get(`/files/share/${accessToken}`);
+    },
+
+    // Download shared file
+    downloadSharedFile: async (accessToken, password = null) => {
+        const config = {
+            responseType: 'blob',
+        };
+        
+        if (password) {
+            return api.post(`/files/share/${accessToken}/download`, { password }, config);
+        } else {
+            return api.get(`/files/share/${accessToken}/download`, config);
+        }
+    },
+};
